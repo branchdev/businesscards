b445f7279a54541ac5b580c15b91e3bdb6c8bc7c
Mike Branchini <mikebranchini@gmail.com>
2013-06-12 01:09:18 -0700

    fix this john

--- protected/migrations/m130612_073231_create_user_table.php
+++ protected/migrations/m130612_073231_create_user_table.php
@@ -4,12 +4,27 @@ class m130612_073231_create_user_table extends CDbMigration
 {
 	public function up()
 	{
+		$this->createTable('tbl_users', array(
+			'affiliate_id'=>'pk',
+			'fname'=>'string NOT NULL',
+			'lname'=>'string NOT NULL',
+			'username'=>'string NOT NULL',
+			'password'=>'string NOT NULL',
+			'address'=>'string NOT NULL',
+			'ein'=>'string',
+			'sponsor_id'=>'string default NULL',
+			'email'=>'string NOT NULL',
+			'phone'=>'string NOT NULL',
+			'created'=>'DATETIME default NULL',
+			'lastvisit'=>'DATETIME default NULL',
+			'status'=>'string NOT NULL',
+			));
 	}
 
 	public function down()
 	{
-		echo "m130612_073231_create_user_table does not support migration down.\n";
-		return false;
+		$this->dropTable('tbl_users');
+		
 	}
 
 	/*
